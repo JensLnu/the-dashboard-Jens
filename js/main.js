883f2fe4060e151175ae9236a7bc782196741125
Jens Bengtsson <jb225je@student.lnu.se>
2023-12-06 16:10:19 +0100

    added functionallity that shows time and date

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
-const time = new Date;
 
-console.log(time)
+document.addEventListener("DOMContentLoaded", addFunctionality);
+
+function addFunctionality() {
+    showClock();
+    showDate();
+    setInterval(showClock, 1000);
+}
+
+function showClock() {
+    const timePlaceholder = document.querySelector('#time-and-date li');
+    const date = new Date;
+    timePlaceholder.textContent = date.toLocaleTimeString();
+}
+
+function showDate() {
+    const datePlaceholder = document.querySelector('#time-and-date li').nextElementSibling;
+    const date = new Date;
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    datePlaceholder.textContent = `${date.getDay()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+}
+
+function changeMenu() {
+    const header = document.querySelector('h1');
+    header.addEventListener('click', () => {
+        
+    });
+}
