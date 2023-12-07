3c66fa065bb7e8422db2c0a16a19d28acf427b98
Jens Bengtsson <jb225je@student.lnu.se>
2023-12-07 08:38:25 +0100

    change header functionality and related css-style and html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+// references
+const header = document.querySelector('.header');
 
 document.addEventListener("DOMContentLoaded", addFunctionality);
 
+// runs sites main functionality
 function addFunctionality() {
     showClock();
     showDate();
     setInterval(showClock, 1000);
+    getCustomHeader();
+    changeHeader();
 }
 
 // gets the time and render it
@@ -22,13 +27,31 @@ function showDate() {
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
-    datePlaceholder.textContent = `${date.getDay()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+    datePlaceholder.textContent = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
 }
 
 // enabel the main header to be changed by the user
-function changeMenu() {
-    const header = document.querySelector('h1');
+function changeHeader() {
     header.addEventListener('click', () => {
-        
+        header.classList.toggle('display-none');
+        header.nextElementSibling.classList.toggle('display-none');
+        header.nextElementSibling.focus();
+        header.nextElementSibling.addEventListener('keydown', saveNewHeader);
     });
+}
+
+// saves the "new" header and displays it
+function saveNewHeader(e) {
+    if (e.keyCode == 13) {
+        localStorage.setItem('customHeader', header.nextElementSibling.value);
+        header.classList.toggle('display-none');
+        header.nextElementSibling.classList.toggle('display-none');
+        getCustomHeader();
+    }
+}
+
+// gets the saved header and renders it
+function getCustomHeader() {
+    const customHeader = localStorage.getItem('customHeader');
+    if (customHeader) header.textContent = customHeader;
 }
