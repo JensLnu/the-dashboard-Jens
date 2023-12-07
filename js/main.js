883561dee1cb92812dfd961ff97a0e182c49d0fe
Jens Bengtsson <jb225je@student.lnu.se>
2023-12-07 07:05:35 +0100

    added comments

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ function addFunctionality() {
     setInterval(showClock, 1000);
 }
 
+// gets the time and render it
 function showClock() {
     const timePlaceholder = document.querySelector('#time-and-date li');
     const date = new Date;
     timePlaceholder.textContent = date.toLocaleTimeString();
 }
 
+// gets the date and render it
 function showDate() {
     const datePlaceholder = document.querySelector('#time-and-date li').nextElementSibling;
     const date = new Date;
@@ -23,6 +25,7 @@ function showDate() {
     datePlaceholder.textContent = `${date.getDay()} ${months[date.getMonth()]} ${date.getFullYear()}`;
 }
 
+// enabel the main header to be changed by the user
 function changeMenu() {
     const header = document.querySelector('h1');
     header.addEventListener('click', () => {
